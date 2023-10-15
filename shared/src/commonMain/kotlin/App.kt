44b2d94eb2cb4d57f65ba9d2e56901079a3bf7d2
anhhuy <phanvietduc1@gmail.com>
2023-10-15 16:26:30 +0700

    call api

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -6,12 +6,19 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -21,6 +28,11 @@ fun App() {
     MaterialTheme {
         var greetingText by remember { mutableStateOf("Hello, World!") }
         var showImage by remember { mutableStateOf(false) }
+
+        LaunchedEffect(Unit) {
+            println("vietDuc: " + getImages())
+        }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
                 greetingText = "Hello, ${getPlatformName()}"
@@ -36,6 +48,18 @@ fun App() {
             }
         }
     }
+}
+
+val httpClient = HttpClient {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+suspend fun getImages() : List<BirdImage>{
+    val images = httpClient
+        .get("https://sebi.io/demo-image-api/pictures.json")
+        .body<List<BirdImage>>()
+    return images
 }
 
 expect fun getPlatformName(): String
